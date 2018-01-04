ab199fd1c5bc21c7418c8e9f30e22d08230e1acf
Veritiu <35099227+Veritium@users.noreply.github.com>
2018-01-04 14:50:29 +0300

    Veritium
    Last version

--- VRTMToken.sol
+++ VRTMToken.sol
@@ -33,7 +33,7 @@ library SafeMath {
     assert(a == 0 || c / a == b);
     return c;
   }
-
+ 
   function div(uint256 a, uint256 b) internal constant returns (uint256) {
     // assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
